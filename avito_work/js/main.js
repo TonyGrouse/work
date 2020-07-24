5522d500f8e6cda32924a8328548a604cb018b83
TonyGrouse <66474959+TonyGrouse@users.noreply.github.com>
2020-07-24 18:43:02 +0300

    Add files via upload

---
+++ avito_work/js/main.js
@@ -0,0 +1,406 @@
+$(document).ready(function() {
+		var w=$(window).outerWidth();
+		var h=$(window).outerHeight();
+		var ua = window.navigator.userAgent;
+		var msie = ua.indexOf("MSIE ");
+		var isMobile = {Android: function() {return navigator.userAgent.match(/Android/i);},BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},Windows: function() {return navigator.userAgent.match(/IEMobile/i);},any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}};
+	function isIE() {
+		ua = navigator.userAgent;
+		var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+		return is_ie; 
+	}
+	if(isIE()){
+		$('body').addClass('ie');
+	}
+	if(isMobile.any()){
+		$('body').addClass('touch');
+	}
+//FORMS
+function forms(){
+	//FIELDS
+	$('input,textarea').focus(function(){
+		if($(this).val() == $(this).attr('data-value')){
+				$(this).addClass('focus');
+				$(this).parent().addClass('focus');
+			if($(this).attr('data-type')=='pass'){
+				$(this).attr('type','password');
+			};
+			$(this).val('');
+		};
+		removeError($(this));
+	});
+	$('input[data-value], textarea[data-value]').each(function() {
+		if (this.value == '' || this.value == $(this).attr('data-value')) {
+			if($(this).hasClass('l') && $(this).parent().find('.form__label').length==0){
+				$(this).parent().append('<div class="form__label">'+$(this).attr('data-value')+'</div>');
+			}else{
+				this.value = $(this).attr('data-value');
+			}
+		}
+		if(this.value!=$(this).attr('data-value') && this.value!=''){
+			$(this).addClass('focus');
+			$(this).parent().addClass('focus');
+			if($(this).hasClass('l') && $(this).parent().find('.form__label').length==0){
+				$(this).parent().append('<div class="form__label">'+$(this).attr('data-value')+'</div>');
+			}
+		}
+
+		$(this).click(function() {
+			if (this.value == $(this).attr('data-value')) {
+				if($(this).attr('data-type')=='pass'){
+					$(this).attr('type','password');
+				};
+				this.value = '';
+			};
+		});
+		$(this).blur(function() {
+			if (this.value == '') {
+				if(!$(this).hasClass('l')){
+					this.value = $(this).attr('data-value');
+				}
+					$(this).removeClass('focus');
+					$(this).parent().removeClass('focus');
+				if($(this).attr('data-type')=='pass'){
+					$(this).attr('type','text');
+				};
+			};
+			if($(this).hasClass('vn')){
+				formValidate($(this));
+			}
+		});
+	});
+	$('.form-input__viewpass').click(function(event) {
+		if($(this).hasClass('active')){
+			$(this).parent().find('input').attr('type','password');
+		}else{
+			$(this).parent().find('input').attr('type','text');
+		}
+		$(this).toggleClass('active');
+	});
+
+	$.each($('input.phone'), function(index, val) {
+		$(this).attr('type','tel');
+		$(this).focus(function(){
+			$(this).inputmask('+7(999) 999 9999',{clearIncomplete: true,clearMaskOnLostFocus: true,
+				"onincomplete": function(){maskclear($(this));}
+			});
+			$(this).addClass('focus');
+			$(this).parent().addClass('focus');
+			$(this).parent().removeClass('err');
+			$(this).removeClass('err');
+		});
+	});
+	$('input.phone').focusout(function(event) {
+		maskclear($(this));
+	});
+	$.each($('input.num'), function(index, val) {
+		$(this).focus(function(){
+			$(this).inputmask('9{1,1000}',{clearIncomplete: true,placeholder:"",clearMaskOnLostFocus: true,"onincomplete": function(){maskclear($(this));}});
+			$(this).addClass('focus');
+			$(this).parent().addClass('focus');
+			$(this).parent().removeClass('err');
+			$(this).removeClass('err');
+		});
+	});
+	$('input.num').focusout(function(event) {
+		maskclear($(this));
+	});
+}
+forms();
+
+function digi(str){
+	var r=str.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+	return r;
+}
+
+//VALIDATE FORMS
+$('form button[type=submit]').click(function(){
+		var er=0;
+		var form=$(this).parents('form');
+		var ms=form.data('ms');
+	$.each(form.find('.req'), function(index, val) {
+		er+=formValidate($(this));
+	});
+	if(er==0){
+		removeFormError(form);
+	
+		if(ms!=null && ms!=''){
+			showMessageByClass(ms);
+			return false;
+		}
+	}else{
+		return false;
+	}
+});
+function formValidate(input){
+		var er=0;
+		var form=input.parents('form');
+	if(input.attr('name')=='email' || input.hasClass('email')){
+		if(input.val()!=input.attr('data-value')){
+			var em=input.val().replace(" ","");
+			input.val(em);
+		}
+		if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.val())) || input.val()==input.attr('data-value')){
+				er++;
+			addError(input);
+		}else{
+			removeError(input);
+		}
+	}else{
+		if(input.val()=='' || input.val()==input.attr('data-value')){
+			er++;
+			addError(input);
+		}else{
+			removeError(input);
+		}
+	}
+	if(input.attr('type')=='checkbox'){
+		if(input.prop('checked') == true){
+			input.removeClass('err').parent().removeClass('err');
+		}else{
+			er++;
+			input.addClass('err').parent().addClass('err');
+		}
+	}
+	if(input.hasClass('name')){
+		if(!(/^[А-Яа-яa-zA-Z-]+( [А-Яа-яa-zA-Z-]+)$/.test(input.val()))){
+				er++;
+			addError(input);
+		}
+	}
+	if(input.hasClass('pass-2')){
+		if(form.find('.pass-1').val()!=form.find('.pass-2').val()){
+			addError(input);
+		}else{
+			removeError(input);
+		}
+	}
+		return er;
+}
+function formLoad(){
+	$('.popup').hide();
+	$('.popup-message-body').hide();
+	$('.popup-message .popup-body').append('<div class="popup-loading"><div class="popup-loading__title">Идет загрузка...</div><div class="popup-loading__icon"></div></div>');
+	$('.popup-message').addClass('active').fadeIn(300);
+}
+function showMessageByClass(ms){
+	$('.popup').hide();
+	popupOpen('message.'+ms,'');
+}
+function showMessage(html){
+	$('.popup-loading').remove();
+	$('.popup-message-body').show().html(html);
+}
+function clearForm(form){
+	$.each(form.find('.input'), function(index, val) {
+			$(this).removeClass('focus').val($(this).data('value'));
+			$(this).parent().removeClass('focus');
+		if($(this).hasClass('phone')){
+			maskclear($(this));
+		}
+	});
+}
+function addError(input){
+		input.addClass('err');
+		input.parent().addClass('err');
+		input.parent().find('.form__error').remove();
+	if(input.hasClass('email')){
+			var error='';
+		if(input.val()=='' || input.val()==input.attr('data-value')){
+			error=input.data('error');
+		}else{
+			error=input.data('error');
+		}
+		if(error!=null){
+			input.parent().append('<div class="form__error">'+error+'</div>');
+		}
+	}else{
+		if(input.data('error')!=null && input.parent().find('.form__error').length==0){
+			input.parent().append('<div class="form__error">'+input.data('error')+'</div>');
+		}
+	}
+	if(input.parents('.select-block').length>0){
+		input.parents('.select-block').parent().addClass('err');
+		input.parents('.select-block').find('.select').addClass('err');
+	}
+}
+function addErrorByName(form,input__name,error_text){
+		var input=form.find('[name="'+input__name+'"]');
+	input.attr('data-error',error_text);
+	addError(input);
+}
+function addFormError(form, error_text){
+	form.find('.form__generalerror').show().html(error_text);
+}
+function removeFormError(form){
+	form.find('.form__generalerror').hide().html('');
+}
+function removeError(input){
+	input.removeClass('err');
+	input.parent().removeClass('err');
+	input.parent().find('.form__error').remove();
+
+	if(input.parents('.select-block').length>0){
+		input.parents('.select-block').parent().removeClass('err');
+		input.parents('.select-block').find('.select').removeClass('err').removeClass('active');
+		//input.parents('.select-block').find('.select-options').hide();
+	}
+}
+function removeFormErrors(form){
+	form.find('.err').removeClass('err');
+	form.find('.form__error').remove();
+}
+function maskclear(n){
+	if(n.val()==""){
+		n.inputmask('remove');
+		if(!n.hasClass('l')){
+			n.val(n.attr('data-value'));
+		}
+		n.removeClass('focus');
+		n.parent().removeClass('focus');
+	}
+}
+var isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
+if (isMobile.any()) { }
+
+if (location.hash) {
+	var hsh = location.hash.replace('#', '');
+	if ($('.popup-' + hsh).length > 0) {
+		popupOpen(hsh);
+	} else if ($('div.' + hsh).length > 0) {
+		$('body,html').animate({ scrollTop: $('div.' + hsh).offset().top, }, 500, function () { });
+	}
+}
+$('.wrapper').addClass('loaded');
+
+var act = "click";
+if (isMobile.iOS()) {
+	var act = "touchstart";
+}
+
+let iconMenu = document.querySelector(".icon-menu");
+let body = document.querySelector("body");
+let menuBody = document.querySelector(".menu__body");
+if (iconMenu) {
+	iconMenu.addEventListener("click", function () {
+		iconMenu.classList.toggle("active");
+		body.classList.toggle("lock");
+		menuBody.classList.toggle("active");
+	});
+}
+
+//Клик вне области
+$(document).on('click touchstart', function (e) {
+	if (!$(e.target).is(".select *")) {
+		$('.select').removeClass('active');
+	};
+});
+
+//UP
+$(window).scroll(function () {
+	var w = $(window).width();
+	if ($(window).scrollTop() > 50) {
+		$('#up').fadeIn(300);
+	} else {
+		$('#up').fadeOut(300);
+	}
+});
+$('#up').click(function (event) {
+	$('body,html').animate({ scrollTop: 0 }, 300);
+});
+
+$('body').on('click', '.tab__navitem', function (event) {
+	var eq = $(this).index();
+	if ($(this).hasClass('parent')) {
+		var eq = $(this).parent().index();
+	}
+	if (!$(this).hasClass('active')) {
+		$(this).closest('.tabs').find('.tab__navitem').removeClass('active');
+		$(this).addClass('active');
+		$(this).closest('.tabs').find('.tab__item').removeClass('active').eq(eq).addClass('active');
+		if ($(this).closest('.tabs').find('.slick-slider').length > 0) {
+			$(this).closest('.tabs').find('.slick-slider').slick('setPosition');
+		}
+	}
+});
+$.each($('.spoller.active'), function (index, val) {
+	$(this).next().show();
+});
+$('body').on('click', '.spoller', function (event) {
+	if ($(this).hasClass('mob') && !isMobile.any()) {
+		return false;
+	}
+
+	if ($(this).parents('.one').length > 0) {
+		$(this).parents('.one').find('.spoller').not($(this)).removeClass('active').next().slideUp(300);
+		$(this).parents('.one').find('.spoller').not($(this)).parent().removeClass('active');
+	}
+
+	if ($(this).hasClass('closeall') && !$(this).hasClass('active')) {
+		$.each($(this).closest('.spollers').find('.spoller'), function (index, val) {
+			$(this).removeClass('active');
+			$(this).next().slideUp(300);
+		});
+	}
+	$(this).toggleClass('active').next().slideToggle(300, function (index, val) {
+		if ($(this).parent().find('.slick-slider').length > 0) {
+			$(this).parent().find('.slick-slider').slick('setPosition');
+		}
+	});
+	return false;
+});
+
+
+
+function scrolloptions() {
+	var scs = 100;
+	var mss = 50;
+	var bns = false;
+	if (isMobile.any()) {
+		scs = 10;
+		mss = 1;
+		bns = true;
+	}
+	var opt = {
+		cursorcolor: "#fff",
+		cursorwidth: "4px",
+		background: "",
+		autohidemode: true,
+		cursoropacitymax: 0.4,
+		bouncescroll: bns,
+		cursorborderradius: "0px",
+		scrollspeed: scs,
+		mousescrollstep: mss,
+		directionlockdeadzone: 0,
+		cursorborder: "0px solid #fff",
+	};
+	return opt;
+}
+function scroll() {
+	$('.scroll-body').niceScroll('.scroll-list', scrolloptions());
+}
+if (navigator.appVersion.indexOf("Mac") != -1) {
+} else {
+	if ($('.scroll-body').length > 0) { scroll(); }
+}
+
+if ($('.t,.tip').length > 0) {
+	tip();
+}
+function tip() {
+	$('.t,.tip').webuiPopover({
+		placement: 'top',
+		trigger: 'hover',
+		backdrop: false,
+		//selector:true,
+		animation: 'fade',
+		dismissible: true,
+		padding: false,
+		//hideEmpty: true
+		onShow: function ($element) { },
+		onHide: function ($element) { },
+	}).on('show.webui.popover hide.webui.popover', function (e) {
+		$(this).toggleClass('active');
+	});
+}
+});
